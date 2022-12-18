9156a6eea6cbe81bfb866feddfb4242d919bbb02
kaipfannebecker <kaipfannebecker1@aol.com>
2022-12-18 17:08:05 +0100

    df_RKI dimensions corrected
    changed dimensions of df_RKI.csv

--- landkreise/Liste_rki.py
+++ landkreise/Liste_rki.py
@@ -15,10 +15,10 @@
 import numpy as np
 import pandas as pd
 
-data = np.array([[11001, 11002, 11003, 11004, 11005, 11006, 11007, 11008, 11009, 11010, 11011, 11012],
-         ["Berlin Mitte", "Berlin Friedrichshain-Kreuzberg", "Berlin Pankow", "Berlin Charlottenburg-Wilmersdorf",
-         "Berlin Spandau", "Berlin Steglitz-Zehlendorf", "Berlin Tempelhof-Schöneberg", "Berlin Neukölln",
-         "Berlin Treptow-Köpenick", "Berlin Marzahn-Hellersdorf", "Berlin Lichtenberg", "Berlin Reinickendorf"]])
+data = np.array([[11001,"Berlin Mitte"], [11002, "Berlin Friedrichshain-Kreuzberg",], [11003, "Berlin Pankow"],
+                 [11004, "Berlin Charlottenburg-Wilmersdorf"],[11005,"Berlin Spandau"], [11006, "Berlin Steglitz-Zehlendorf"],
+                 [11007, "Berlin Tempelhof-Schöneberg"], [11008,"Berlin Neukölln"], [11009, "Berlin Treptow-Köpenick"],
+                 [11010,"Berlin Marzahn-Hellersdorf"], [11011,"Berlin Lichtenberg"], [11012, "Berlin Reinickendorf"]])
 
 #print(data)
 #np.savetxt("data.csv", data, delimiter=",")

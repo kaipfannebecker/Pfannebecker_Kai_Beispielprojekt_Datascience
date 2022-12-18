6e7e6040ccb4265bec38892f1635137d27b4343b
kaipfannebecker <kaipfannebecker1@aol.com>
2022-12-18 16:13:44 +0100

    Generation of liste_rki
    Generation of dataframe liste_rki moved to a seperate file

--- landkreise/liste_landkreise.py
+++ landkreise/liste_landkreise.py
@@ -18,29 +18,8 @@ lk_fin = lk_small
 # csv ausgeben
 lk_fin.to_csv("lk_fin.csv")
 
-# Hinzufügen von RKI spezifischen Landkreisen
-## 11001 Berlin Mitte
-## 11002 Berlin Friedrichshain-Kreuzberg
-## 11003 Berlin Pankow
-## 11004 Berlin Charlottenburg-Wilmersdorf
-## 11005 Berlin Spandau
-## 11006 Berlin Steglitz-Zehlendorf
-## 11007 Berlin Tempelhof-Schöneberg
-## 11008 Berlin Neukölln
-## 11009 Berlin Treptow-Köpenick
-## 11010 Berlin Marzahn-Hellersdorf
-## 11011 Berlin Lichtenberg
-## 11012 Berlin Reinickendorf
-
-df_RKI = data.frame[(11001, 11002, 11003, 11004, 11005, 11006, 11007, 11008, 11009, 11010, 11011, 11012,),
-        ("Berlin Mitte", "Berlin Friedrichshain-Kreuzberg", "Berlin Pankow", "Berlin Charlottenburg-Wilmersdorf",
-         "Berlin Spandau", "Berlin Steglitz-Zehlendorf", "Berlin Tempelhof-Schöneberg", "Berlin Neukölln",
-         "Berlin Treptow-Köpenick", "Berlin Marzahn-Hellersdorf", "Berlin Lichtenberg", "Berlin Reinickendorf")]
-
-df_RKI.to_csv("lk_fin.csv", mode='a')
-
 # als csv Datei ausgeben
-lk_fin.to_csv("landkreise.csv")
+# lk_fin.to_csv("landkreise.csv")
 
 ## Delete rows where case numbers are zero
 ## This deletion is completed by "selecting" rows where case numbers are non zero

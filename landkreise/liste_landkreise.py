a7f961406fbf5fe9b7fd40c3731e296d670fb036
kaipfannebecker <kaipfannebecker1@aol.com>
2022-12-20 12:30:28 +0100

    combining Values works
    Die Identifier werden korrekt kombiniert

--- landkreise/liste_landkreise.py
+++ landkreise/liste_landkreise.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Ließt Idenitifier und Namen der Landkreise ein
+# Ließt Idenitifier und Namen der Landkreise ein; ACHTUNG: resultierender Datatype String!
 ##lk_all = pd.read_excel("AuszugGV2QAktuell.xlsx", sheet_name=1, header=[0, 1, 2, 3, 4, 5], na_values=['NA'],
                       #usecols=["C, D, E, F, H"], skiprows=[0, 1, 2, 4])
 lk_all = pd.read_excel("AuszugGV2QAktuell.xlsx", sheet_name=1, header=6, usecols='C:F, H',
@@ -9,43 +9,32 @@ lk_all = pd.read_excel("AuszugGV2QAktuell.xlsx", sheet_name=1, header=6, usecols
 #lk_all.to_csv("liste_LK.csv")
 print(lk_all)
 
-#df = pd.read_excel("test.xlsx",
-#                   sheet_name="test",
-#                   header=2,
-#                   index_col=[0,1],
-#                   usecols='A:B,D:G',
-#                   parse_dates=False)
-#index = pd.read_excel("test.xlsx",
-#                      sheet_name="test",
-#                      header=None,
-#                      skiprows=1,
-#                      index_col=[0,1],
-#                      nrows=2,
-#                      usecols='A:B,D:G',
-#                      parse_dates=False)
-#index = index.fillna(method='ffill', axis=1)
-#df.columns = pd.MultiIndex.from_arrays(index.values)
-#df
+#lk_all = lk_all.drop(index=[0], columns=[0])
+#print(lk_all)
 
-
-
-
-
-
-
-# kopiert alle Landkreise (d. h. alle Einträge mit NaN in VB) in neue Datei
+## kopiert alle Landkreise (d. h. alle Einträge mit NaN in VB) in neue Datei
 #lk_big = lk_all[lk_all['VB'].null()]
-lk_big = lk_all.drop([nonull:=True], implace=False)
+#lk_big = lk_all.drop([nonull:=True], inplace=False)
 
-# löscht alle Zeilen, in denen die Spalte VB ausgefüllt ist
-lk_small = lk_big.loc(lk_big["VB"])
+#print(lk_big)
 
-# Zellen C, D, E zu einer Nummer verbinden
+## löscht alle Zeilen, in denen die Spalte VB ausgefüllt ist
+#lk_small = lk_big.loc(lk_big["VB"])
 
-lk_fin = lk_small
+## Zellen C, D, E zu einer Nummer verbinden
 
-# csv ausgeben
-lk_fin.to_csv("lk_fin.csv")
+zweiter_wert = lk_all["Unnamed: 3"]
+dritter_wert = lk_all["Unnamed: 4"]
+
+lk_all["01"] = lk_all["01"].str.cat(zweiter_wert)
+lk_all["01"] = lk_all["01"].str.cat(dritter_wert)
+
+
+print(lk_all)
+#lk_fin = lk_small
+
+## csv ausgeben
+#lk_fin.to_csv("lk_fin.csv")
 
 # als csv Datei ausgeben
 # lk_fin.to_csv("landkreise.csv")

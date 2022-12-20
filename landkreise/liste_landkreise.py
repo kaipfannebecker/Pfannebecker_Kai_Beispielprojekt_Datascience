5079a57fe8cfb0a03c1bd170f02114aea6f1cfa4
kaipfannebecker <kaipfannebecker1@aol.com>
2022-12-20 10:42:17 +0100

    import von Excel funktioniert
    Import von Excel Funktioniert; frontale nullen werden noch gelöscht

--- landkreise/liste_landkreise.py
+++ landkreise/liste_landkreise.py
@@ -1,10 +1,37 @@
 import pandas as pd
 
 # Ließt Idenitifier und Namen der Landkreise ein
-lk_all = pd.read_excel("AuszugGV2QAktuell.xlsx", sheet_name=1, header=[0, 1, 2, 3, 4, 5], na_values=['NA'],
-                      usecols=["C, D, E, F, H"], skiprows=[0, 1, 2, 4])
+##lk_all = pd.read_excel("AuszugGV2QAktuell.xlsx", sheet_name=1, header=[0, 1, 2, 3, 4, 5], na_values=['NA'],
+                      #usecols=["C, D, E, F, H"], skiprows=[0, 1, 2, 4])
+lk_all = pd.read_excel("AuszugGV2QAktuell.xlsx", sheet_name=1, header=6, usecols='C:F, H',
+                      na_values=['NA'])
 
+lk_all.to_csv("liste_LK.csv")
 print(lk_all)
+
+#df = pd.read_excel("test.xlsx",
+#                   sheet_name="test",
+#                   header=2,
+#                   index_col=[0,1],
+#                   usecols='A:B,D:G',
+#                   parse_dates=False)
+#index = pd.read_excel("test.xlsx",
+#                      sheet_name="test",
+#                      header=None,
+#                      skiprows=1,
+#                      index_col=[0,1],
+#                      nrows=2,
+#                      usecols='A:B,D:G',
+#                      parse_dates=False)
+#index = index.fillna(method='ffill', axis=1)
+#df.columns = pd.MultiIndex.from_arrays(index.values)
+#df
+
+
+
+
+
+
 
 # kopiert alle Landkreise (d. h. alle Einträge mit NaN in VB) in neue Datei
 #lk_big = lk_all[lk_all['VB'].null()]

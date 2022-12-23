a0c0f7df74ec458b7ab1753422e576250ba9381b
kaipfannebecker <kaipfannebecker1@aol.com>
2022-12-23 13:22:45 +0100

    RKI Daten live einpflegen

--- rki_daten/Filterung_Datensaetze.py
+++ rki_daten/Filterung_Datensaetze.py
@@ -1,8 +1,21 @@
 import pandas as pd
-import numpy as np
+from datetime import date
+# import numpy as np
+# import calendar
 
-#dataset_rki = []
-dataset_rki = pd.read_csv('.\\Datensatz_gesamt\\2021-04-02_Deutschland_SarsCov2_Infektionen.csv')
+# today = date.today()
+# date_today = today.strftime("%Y-%m-%d")
+# print(date_today)
+
+## aktuellen Datensatz des RKI importieren
+
+dataset_rki = pd.read_csv('https://github.com/robert-koch-institut/SARS-CoV-2-Infektionen_in_Deutschland/blob/main/Aktuell_Deutschland_SarsCov2_Infektionen.csv')
+
+# dataset_rki = pd.read_csv(f'https://github.com/robert-koch-institut/SARS-CoV-2-Infektionen_in_Deutschland_Archiv/blob/master/Archiv/{date_today}_Deutschland_SarsCov2_Infektionen.csv')
+
+
+# dataset_rki = []
+# dataset_rki = pd.read_csv('.\\Datensatz_gesamt\\2021-04-02_Deutschland_SarsCov2_Infektionen.csv')
 tab_lk = pd.read_csv(r'C:\Users\Kai\Desktop\Projekt_Datascience\Liste_der_Landkreise_fuer_Projekt.csv')
 
 # groupby the desired column and iterate through the groupby object
@@ -14,4 +27,4 @@ for Id_lk, dataset_rki in dataset_rki.groupby('IdLandkreis'):
     # save the dataframe for each group to a csv
     dataset_rki.to_csv(f'.\\Datensatz_vereinzelt\\by_name\\{name_lk}.csv', index=False, mode='a')
     dataset_rki.to_csv(f'.\\Datensatz_vereinzelt\\by_number\\{number_lk}.csv', index=False, mode='a')
-    dataset_rki.to_csv(f'.\\Datensatz_vereinzelt\\by_number_name\\{number_lk}_{name_lk}.csv', index=False, mode ='a')
+    dataset_rki.to_csv(f'.\\Datensatz_vereinzelt\\by_number_name\\{number_lk}_{name_lk}.csv', index=False, mode='a')
